9f04d61f7ea84fa564c2119a69d1c9a8355dd8ac
Muhammad Yusron <muhammadyusron17@gmail.com>
2020-08-21 07:02:44 +0700

    minimize navbar

--- application/language/indonesian/services_lang.php
+++ application/language/indonesian/services_lang.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 $lang['service_1_title'] = 'Marine Supply';
 
@@ -14,8 +14,8 @@ $lang['service_5_title'] = 'Services And Installation';
 
 #################################
 
-$lang['service_1_desc'] = 'High quality with Safety Of Life At Sea (SOLAS) standard: <span class="collapse" id="mrcollapse">
-<ul style="list-style: circle;text-align: left;">
+$lang['service_1_desc'] = 'High quality with Safety Of Life At Sea (SOLAS) standard: 
+  <ul class="py-2" style="list-style: inside;text-align: left;">
     <li>Lashing equipment (Turn buckle, Tensioner, e.g., chain, ratchet,etc.)</li>
     <li>Deck (Pilot - Embarkation ladder, Tail rope, Mooring rope,etc.)</li>
     <li>Bunker, fresh water supply,etc.</li>
@@ -26,11 +26,10 @@ $lang['service_1_desc'] = 'High quality with Safety Of Life At Sea (SOLAS) stand
     <li>Life safety and firefightingequipment</li>
     <li>Charts/publication</li>
     <li>Chemical product for maintenance, water/oil treatment, cleaning, pollutioncontrol.</li>
-</ul>
-</span> <a class="collapers btn btn-warning btn-sm " href="#" data-toggle="collapse" data-target="#mrcollapse" aria-expanded="false" aria-controls="mrcollapse">Read More..</a>';
+</ul>';
 
-$lang['service_2_desc'] = 'The following are a brief outline of some of the various types of survey and consultancy work undertaken <span class="collapse" id="spcollapse">
-<ul style="list-style: circle;text-align: left;">
+$lang['service_2_desc'] = 'The following are a brief outline of some of the various types of survey and consultancy work undertaken 
+<ul class="py-2" style="list-style: inside;text-align: left;">
     <li>DraughtSurveys</li>
     <li>On and Off HireSurveys</li>
     <li>Hatch and Tank CleanlinessSurveys.</li>
@@ -41,12 +40,13 @@ $lang['service_2_desc'] = 'The following are a brief outline of some of the vari
     <li>Underwater Hull Inspections and/or CleaningSupervision.</li>
     <li>Flag State Annual SafetyInspections</li>
 </ul>
-</span><a class="collapers btn btn-warning btn-sm " href="#" data-toggle="collapse" data-target="#spcollapse" aria-expanded="false" aria-controls="spcollapse">Read More..</a>';
+';
 
-$lang['service_3_desc'] = 'Agency service is one of PT. SAGARA BERKAH MARINE service consist of shipping agency for domestic and foreign ships,supported by our experienced personnel throughout Indonesia. <span class="collapse" id="itcollapse">We are growing and expanding our businesses in Indonesia by providing a full range of shipping agency for all types of vessels such as Tanker vessels, Container vessels, Bulk vessels, General cargo vessels, roll on – roll off (RORO) vessels andothers.</span><a class="collapers btn btn-warning btn-sm " href="#" data-toggle="collapse" data-target="#itcollapse" aria-expanded="false" aria-controls="itcollapse">Read More..</a>';
+$lang['service_3_desc'] = 'Agency service is one of PT. SAGARA BERKAH MARINE service consist of shipping agency for domestic and foreign ships,supported by our experienced personnel throughout Indonesia. 
+We are growing and expanding our businesses in Indonesia by providing a full range of shipping agency for all types of vessels such as Tanker vessels, Container vessels, Bulk vessels, General cargo vessels, roll on – roll off (RORO) vessels andothers.';
 
-$lang['service_4_desc'] = 'With highly experienced and full skilled technician/engineer in each section which will solve the problem on board. <span class="collapse" id="recollapse">
-<ul style="list-style: circle;text-align: left;">
+$lang['service_4_desc'] = 'With highly experienced and full skilled technician/engineer in each section which will solve the problem on board. 
+<ul class="py-2" style="list-style: inside;text-align: left;">
     <li>EngineOverhaul</li>
     <li>Electro Motorrepair/rewind</li>
     <li>Trouble Shooting Electric</li>
@@ -56,39 +56,65 @@ $lang['service_4_desc'] = 'With highly experienced and full skilled technician/e
     <li>Pump.</li>
     <li>Crane</li>
     <li>Etc.</li>
-</ul>
-</span><a class="collapers btn btn-warning btn-sm " href="#" data-toggle="collapse" data-target="#recollapse" aria-expanded="false" aria-controls="recollapse">Read More..</a>';
+</ul>';
 
-$lang['service_5_desc'] = 'Engineering – Constructions - Mechanical <span class="collapse" id="s5collapse">
-<ul style="list-style: circle;text-align: left;">
+$lang['service_5_desc'] = 'Engineering – Constructions - Mechanical 
+<ul class="py-2" style="list-style: inside;text-align: left;">
     <li>Under Water Survey/Inspection, UWILD, Cleaning, Polishing Propeller (Class Approval), Searching the sank of anchor/chain/grab bucket, repairs (welding/un welding), Salvage, etc.</li>
     <li>Generator/Electrical, Motorrepair/rewind</li>
     <li>Survey - annual, intermediate and special periodical</li>
     <li>All kind of Test load, ThicknessMeasurement</li>
     <li>Calibration - Repair:
-		<ul>
+		<ul class="py-2">
 			<li>Electrical	: Ampere meter AC/DC, Clamp Meter, Voltmeter, Multi meter, etc. Temperature	: Incubator, Oven, Furnace, Auto Clave,etc.</li>
 			<li>Geodetic	: Automatic Level, Theodolite, Total Station, etc. Pressure	: Pressure Gauge, Pressure Recorder, Test Gauge, etc. Dimension	: Dial Indicator, Vernier Caliper, Height Gauge,etc.</li>
 		</ul>
     </li>
     <li>Conduct annual servicing, inspection and maintenance of FFA and LSA equipment class BV/LR/NK:
-		<ul>
+		<ul class="py-2">
 			<li>Fire extinguishers, CO2 installations</li>
 			<li>Annual foam compound analysis for fixed foam firefighting installation</li>
 			<li>Annual services and maintenance of lifeboat, life raft and launching appliances</li>
 		</ul>
     </li>
     <li>Navigation and communication: Radar and ARPASystem
-		<ul>
+		<ul class="py-2">
 			<li>VDR System (Voyage Data Recorder) Gyro Compass and Magnetic Compass Auto Pilot</li>
 			<li>Inmarsat – A, B, C, F, M</li>
 			<li>VHF DSC, MF/HF DSC, SSB Radio Station</li>
 			<li>GMDSS System - AIS / GPS / ECDIS / EPIRB / Telex / Navtex V-SAT – Broadband or Thuraya</li>
 		</ul>
     </li>
-</ul>
-</span><a class="collapers btn btn-warning btn-sm " href="#" data-toggle="collapse" data-target="#s5collapse" aria-expanded="false" aria-controls="s5collapse">Read More..</a>';
+</ul>';
+
+// $lang['service_5_desc'] = 'Engineering – Constructions - Mechanical <span class="collapse" id="s5collapse">
+// <ul class="py-2" style="list-style: inside;text-align: left;">
+//     <li>Under Water Survey/Inspection, UWILD, Cleaning, Polishing Propeller (Class Approval), Searching the sank of anchor/chain/grab bucket, repairs (welding/un welding), Salvage, etc.</li>
+//     <li>Generator/Electrical, Motorrepair/rewind</li>
+//     <li>Survey - annual, intermediate and special periodical</li>
+//     <li>All kind of Test load, ThicknessMeasurement</li>
+//     <li>Calibration - Repair:
+// 		<ul>
+// 			<li>Electrical	: Ampere meter AC/DC, Clamp Meter, Voltmeter, Multi meter, etc. Temperature	: Incubator, Oven, Furnace, Auto Clave,etc.</li>
+// 			<li>Geodetic	: Automatic Level, Theodolite, Total Station, etc. Pressure	: Pressure Gauge, Pressure Recorder, Test Gauge, etc. Dimension	: Dial Indicator, Vernier Caliper, Height Gauge,etc.</li>
+// 		</ul>
+//     </li>
+//     <li>Conduct annual servicing, inspection and maintenance of FFA and LSA equipment class BV/LR/NK:
+// 		<ul>
+// 			<li>Fire extinguishers, CO2 installations</li>
+// 			<li>Annual foam compound analysis for fixed foam firefighting installation</li>
+// 			<li>Annual services and maintenance of lifeboat, life raft and launching appliances</li>
+// 		</ul>
+//     </li>
+//     <li>Navigation and communication: Radar and ARPASystem
+// 		<ul>
+// 			<li>VDR System (Voyage Data Recorder) Gyro Compass and Magnetic Compass Auto Pilot</li>
+// 			<li>Inmarsat – A, B, C, F, M</li>
+// 			<li>VHF DSC, MF/HF DSC, SSB Radio Station</li>
+// 			<li>GMDSS System - AIS / GPS / ECDIS / EPIRB / Telex / Navtex V-SAT – Broadband or Thuraya</li>
+// 		</ul>
+//     </li>
+// </ul>
+// </span><a class="collapers btn btn-warning btn-sm " href="#" data-toggle="collapse" data-target="#s5collapse" aria-expanded="false" aria-controls="s5collapse">Read More..</a>';
 
 $lang['business_title'] = 'Bisnis';
-
- 
--- application/views/home/home_v.php
+++ application/views/home/home_v.php
@@ -1,24 +1,24 @@
+<div id="home" class="carousel slide padtopslider section " data-ride="carousel" data-interval="5000">
 
 
-<div id="home" class="carousel slide padtopslider section " data-ride="carousel" data-interval="5000"> 
+    <div class="carousel-inner">
+        <?php $c = 0;
+        foreach ($slider as $slide) : ?>
+            <div class="carousel-item <?= ($c == 0) ? 'active' : '' ?>">
+                <img src="<?= base_url() ?><?= $slide ?>" class="d-block w-100" alt="PT Rajawali Cahaya Mandiri">
+            </div>
+        <?php $c++;
+        endforeach ?>
+    </div>
 
-
-  <div class="carousel-inner">
-    <?php $c = 0; foreach ($slider as $slide ): ?>
-        <div class="carousel-item <?=($c==0)?'active':''?>">
-          <img src="<?=base_url()?><?=$slide?>" class="d-block w-100" alt="PT Rajawali Cahaya Mandiri">
-        </div>
-    <?php $c++; endforeach ?>
-  </div>
-
-  <a class="carousel-control-prev" href="#home" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#home" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+    <a class="carousel-control-prev" href="#home" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#home" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
 
 <!-- discover area start  -->
@@ -27,104 +27,107 @@
         <div class="row justify-content-center">
             <div class="col-lg-8">
                 <div class="section-title">
-                    <h2 class="title"><?=$this->lang->line('business_title') ?></h2>
+                    <h2 class="title"><?= $this->lang->line('business_title') ?></h2>
                 </div>
             </div>
         </div>
         <div class="row justify-content-center">
-            
-            <div class="col-sm-4 my-5 px-5 " >
+
+            <div class="col-sm-4 my-5 ">
                 <div class="mycard">
                     <div class="single-discover-item">
-                        <div class ="icon myimg-top-overflow">
+                        <div class="icon myimg-top-overflow">
                             <!-- <i class="flaticon-chat"></i> -->
-                            <img src="<?=base_url()?>assets/images/featured/life-guard.png" alt="">
+                            <img src="<?= base_url() ?>assets/images/featured/life-guard.png" alt="">
                         </div>
                         <div class="content mygoing-up">
-                            <h4 class="title"><?=$this->lang->line('service_1_title')?></h4>
+                            <h4 class="title"><?= $this->lang->line('service_1_title') ?></h4>
+                            <div style="text-align: left;">
+                                <?= $this->lang->line('service_1_desc') ?>
+                            </div>
 
-                            <?=$this->lang->line('service_1_desc')?>
 
                         </div>
                     </div>
                 </div>
-                
+
             </div>
-            <div class="col-sm-4  my-5 px-5">
+            <div class="col-sm-4  my-5">
                 <div class="mycard">
                     <div class="single-discover-item">
-                        <div class ="icon myimg-top-overflow">
+                        <div class="icon myimg-top-overflow">
                             <!-- <i class="flaticon-shield-1"></i> -->
-                            <img src="<?=base_url()?>assets/images/featured/technical-support.png" alt="">
+                            <img src="<?= base_url() ?>assets/images/featured/technical-support.png" alt="">
                         </div>
                         <div class="content mygoing-up">
-                            <h4 class="title"><?=$this->lang->line('service_2_title')?></h4>
-
-                            <?=$this->lang->line('service_2_desc')?>
-            
-
+                            <h4 class="title"><?= $this->lang->line('service_2_title') ?></h4>
+                            <div style="text-align: left;">
+                                <?= $this->lang->line('service_2_desc') ?>
+                            </div>
                         </div>
                     </div>
                 </div>
-                
+
             </div>
-            <div class="col-sm-4  my-5 px-5">
+            <div class="col-sm-4  my-5">
                 <div class="mycard">
                     <div class="single-discover-item">
-                        <div class ="icon myimg-top-overflow">
+                        <div class="icon myimg-top-overflow">
                             <!-- <i class="flaticon-cloud"></i> -->
-                            <img src="<?=base_url()?>assets/images/featured/boat.png" alt="">
+                            <img src="<?= base_url() ?>assets/images/featured/boat.png" alt="">
                         </div>
                         <div class="content mygoing-up">
-                            <h4 class="title"><?=$this->lang->line('service_3_title')?></h4>
-
-                            <?=$this->lang->line('service_3_desc')?>
-
+                            <h4 class="title"><?= $this->lang->line('service_3_title') ?></h4>
+                            <div style="text-align: left;">
+                                <?= $this->lang->line('service_3_desc') ?>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
 
-                </div>
-                
-            <div class="col-sm-4  my-5 px-5">
+            <div class="col-sm-6  my-5">
                 <div class="mycard">
                     <div class="single-discover-item">
-                        <div class ="icon myimg-top-overflow">
+                        <div class="icon myimg-top-overflow">
                             <!-- <i class="flaticon-layers-2"></i> -->
-                            <img src="<?=base_url()?>assets/images/featured/toolbox.png" alt="">
+                            <img src="<?= base_url() ?>assets/images/featured/toolbox.png" alt="">
                         </div>
                         <div class="content mygoing-up">
-                            <h4 class="title"><?=$this->lang->line('service_4_title')?></h4>
+                            <h4 class="title"><?= $this->lang->line('service_4_title') ?></h4>
+                            <div style="text-align: left;">
+                                <?= $this->lang->line('service_4_desc') ?>
+                            </div>
 
-                            <?=$this->lang->line('service_4_desc')?>
 
                         </div>
                     </div>
                 </div>
 
-                </div>
-                
-            <div class="col-sm-4  my-5 px-5">
+            </div>
+
+            <div class="col-sm-6  my-5">
                 <div class="mycard">
                     <div class="single-discover-item">
-                    <div class="icon myimg-top-overflow">
+                        <div class="icon myimg-top-overflow">
                             <!--  <i class="flaticon-layers-2"></i> -->
-                            <img src="<?=base_url()?>assets/images/featured/service-maintenance1.png" alt="">
+                            <img src="<?= base_url() ?>assets/images/featured/service-maintenance1.png" alt="">
                         </div>
                         <div class="content mygoing-up">
-                            <h4 class="title"><?=$this->lang->line('service_5_title')?></h4>
-
-                            <?=$this->lang->line('service_5_desc')?>
+                            <h4 class="title"><?= $this->lang->line('service_5_title') ?></h4>
+                            <div style="text-align: left;">
+                                <?= $this->lang->line('service_5_desc') ?>
+                            </div>
 
                         </div>
                     </div>
                 </div>
             </div>
-    </div>
+        </div>
 </section>
 
-<section class="discover-area padding-top-110 section" id="profile" >
-    <div class="pt-3" style="background-image: url('<?=base_url()?>/assets/images/featured/profile.jpg');padding-bottom:40px;  
+<section class="discover-area padding-top-110 section" id="profile">
+    <div class="pt-3" style="background-image: url('<?= base_url() ?>/assets/images/featured/profile.jpg');padding-bottom:40px;  
     /*parallax*/
   min-height: 400px; 
   background-attachment: fixed;
@@ -135,27 +138,27 @@
             <div class="row justify-content-center">
                 <div class="col-12" style="text-align: center;">
 
-                    <h3 style="text-decoration: underline;"><?=$this->lang->line('about_us_title')?></h3>                
-                    <p><?=$this->lang->line('about_us_desc')?></p>
+                    <h3 style="text-decoration: underline;"><?= $this->lang->line('about_us_title') ?></h3>
+                    <p><?= $this->lang->line('about_us_desc') ?></p>
 
                 </div>
             </div>
             <div class="row" style="display: none">
-                <div class="col-md-6 m-auto" >                    
-                    
-                    <h5 style="text-align:center;"><?=$this->lang->line('about_us_title')?></h5>
-                    <p><?=substr($this->lang->line('about_us_desc'), 0,96 ) ?> ...</p>
+                <div class="col-md-6 m-auto">
+
+                    <h5 style="text-align:center;"><?= $this->lang->line('about_us_title') ?></h5>
+                    <p><?= substr($this->lang->line('about_us_desc'), 0, 96) ?> ...</p>
                 </div>
-                <div class="col-md-6" >  
-                    <img src="<?=base_url()?>/assets/images/featured/profile.jpg" alt="Image Profile"> 
+                <div class="col-md-6">
+                    <img src="<?= base_url() ?>/assets/images/featured/profile.jpg" alt="Image Profile">
                 </div>
             </div>
         </div>
         <div style="text-align: center">
-            <a href="<?=base_url()?>profile" class="btn btn-primary"><i>Read More... </i></a>
+            <a href="<?= base_url() ?>profile" class="btn btn-primary"><i>Read More... </i></a>
         </div>
     </div>
-    
+
 </section>
 
 <!-- discover area start  -->
@@ -164,22 +167,22 @@
     <div class="container my-5">
         <div class="row justify-content-center">
             <div class="col-12" style="text-align: center;">
-                <h3 style="text-decoration: underline;"><?=$this->lang->line('profile_title')?></h3>
+                <h3 style="text-decoration: underline;"><?= $this->lang->line('profile_title') ?></h3>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6 m-auto" >                    
-                
-                <h5 style="text-align:center;"><?=$this->lang->line('about_us_title')?></h5>
-                <p><?=substr($this->lang->line('about_us_desc'), 0,96 ) ?> ...</p>
+            <div class="col-md-6 m-auto">
+
+                <h5 style="text-align:center;"><?= $this->lang->line('about_us_title') ?></h5>
+                <p><?= substr($this->lang->line('about_us_desc'), 0, 96) ?> ...</p>
             </div>
-            <div class="col-md-6" >  
-                <img src="<?=base_url()?>/assets/images/featured/profile.jpg" alt="Image Profile"> 
+            <div class="col-md-6">
+                <img src="<?= base_url() ?>/assets/images/featured/profile.jpg" alt="Image Profile">
             </div>
         </div>
     </div>
     <div style="text-align: center">
-        <a href="<?=base_url()?>profile" class="btn btn-primary"><i>Read More... </i></a>
+        <a href="<?= base_url() ?>profile" class="btn btn-primary"><i>Read More... </i></a>
     </div>
 </section>
 
@@ -188,18 +191,19 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6">
-                <div class="contact-area-wrapper" ><!-- contact area wrapper -->
-                    <span class="subtitle"><?=$this->lang->line('contact')?></span>
+                <div class="contact-area-wrapper">
+                    <!-- contact area wrapper -->
+                    <span class="subtitle"><?= $this->lang->line('contact') ?></span>
                     <div class="row">
                         <div class="col-4">
-                            <?=$this->lang->line('contact_phone')?>
+                            <?= $this->lang->line('contact_phone') ?>
                         </div>
 
                         <div class="col-1">
                             :
                         </div>
                         <div class="col-7">
-                           +62 21-6509087<br>+62 21-6509086
+                            +62 21-6509087<br>+62 21-6509086
                         </div>
                     </div>
                     <div class="row">
@@ -215,7 +219,7 @@
                     </div>
                     <div class="row">
                         <div class="col-4">
-                           <?=$this->lang->line('profile_address')?>
+                            <?= $this->lang->line('profile_address') ?>
                         </div>
                         <div class="col-1">
                             :
@@ -226,20 +230,20 @@
                             Tanjung Priok, Jakarta Utara 14350
                         </div>
                     </div>
-                    
+
                     &nbsp;
                     &nbsp;
                     &nbsp;
-                    <p><?=$this->lang->line('ContactDesc')?></p>
-                    <a href="<?=base_url()?>/contact-us" class="btn btn-lg btn-primary"><i class="fa fa-envelope"></i> Get In Touch</a>
+                    <p><?= $this->lang->line('ContactDesc') ?></p>
+                    <a href="<?= base_url() ?>/contact-us" class="btn btn-lg btn-primary"><i class="fa fa-envelope"></i> Get In Touch</a>
                 </div><!-- //. contact area wrapper -->
             </div>
             <div class="col-lg-6">
                 <div class="img-wrapper">
-                    <img style="max-height: 450px" src="<?=base_url()?>assets/images/contact/contact-us.png" alt="">
+                    <img style="max-height: 450px" src="<?= base_url() ?>assets/images/contact/contact-us.png" alt="">
                     <a target="_blank" href="http://www.freepik.com">
                         <!-- <small style="color:#C1C1C1">Designed by pikisuperstar / Freepik</small> -->
-                    </a>   
+                    </a>
                 </div>
             </div>
         </div>
@@ -247,22 +251,17 @@
 </div>
 
 <script>
-
-    $('.collapse').on('shown.bs.collapse', function () {
-       $(this).next().text('Less More..')
+    $('.collapse').on('shown.bs.collapse', function() {
+        $(this).next().text('Less More..')
 
     })
 
-    $('.collapse').on('hidden.bs.collapse', function () {
+    $('.collapse').on('hidden.bs.collapse', function() {
         $(this).next().text('Read More..')
     })
-
 </script>
 
 &nbsp;
 &nbsp;
 &nbsp;
 &nbsp;
-
-
-
