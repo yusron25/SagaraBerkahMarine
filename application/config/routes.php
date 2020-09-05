ae59389a0dc4282544ae482313fdfb801610037e
Muhammad Yusron <muhammad.yusron@gmail.com>
2020-09-05 23:38:50 +0700

    as

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,10 +1,10 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+$route['default_controller'] = 'Home';
 $route['our-business'] = 'Business';
 $route['about-us'] = 'About';
 $route['contact'] = 'Contact/index';
 
-$route['default_controller'] = 'Home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
