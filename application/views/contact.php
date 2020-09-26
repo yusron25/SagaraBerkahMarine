027784efdee1d51ca8855a37baa781b43601c30d
Muhammad Yusron <muhammadyusron17@gmail.com>
2020-09-26 17:20:15 +0700

    mapss

--- application/views/contact.php
+++ application/views/contact.php
@@ -7,17 +7,9 @@
         </div>
 
         <div class="row" data-aos="fade-up" data-aos-delay="100">
-            <div class="col-lg-6">
-                <div class="info-box mb-4">
-                    <i class="bx bx-map" style="color: goldenrod;"></i>
-                    <h3>Our Address</h3>
-                    <p> KIRANA 2 TOWER, Floor 10th –A<br>
-                        JL. Boulevard Timur No. 88 <br>
-                        Kelapa Gading - Jakarta Utara 14240</p>
-                </div>
-            </div>
 
-            <div class="col-lg-3 col-md-6">
+
+            <div class="col-lg-6 col-md-6">
                 <div class="info-box  mb-4">
                     <i class="bx bx-envelope" style="color: goldenrod;"></i>
                     <h3>Email Us</h3>
@@ -25,7 +17,7 @@
                 </div>
             </div>
 
-            <div class="col-lg-3 col-md-6">
+            <div class="col-lg-6 col-md-6">
                 <div class="info-box  mb-4">
                     <i class="bx bx-phone-call" style="color: goldenrod;"></i>
                     <h3>Call Us</h3>
@@ -38,59 +30,74 @@
         </div>
 
         <div class="row" data-aos="fade-up" data-aos-delay="100">
-
-            <div class="col-lg-6 ">
-                <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.7436271309407!2d106.91210551432313!3d-6.165077895536542!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698ad2eef61e0f%3A0xcc11b45554e76f6f!2sKirana%20Two%20Office%20Tower!5e0!3m2!1sid!2sid!4v1599321604439!5m2!1sid!2sid" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
-            </div>
-
-            <div class="col-lg-6" style="display: none;">
-                <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                    <div class="form-row">
-                        <div class="col form-group">
-                            <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                            <div class="validate"></div>
-                        </div>
-                        <div class="col form-group">
-                            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                            <div class="validate"></div>
+            <div class="col-sm-6">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="info-box mb-4">
+                            <i class="bx bx-map" style="color: goldenrod;"></i>
+                            <h3>Our Address</h3>
+                            <p> KIRANA 2 TOWER, Floor 10th –A<br>
+                                JL. Boulevard Timur No. 88 <br>
+                                Kelapa Gading - Jakarta Utara 14240</p>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                        <div class="validate"></div>
+
+                    <div class="col-lg-12 ">
+                        <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.7436271309407!2d106.91210551432313!3d-6.165077895536542!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698ad2eef61e0f%3A0xcc11b45554e76f6f!2sKirana%20Two%20Office%20Tower!5e0!3m2!1sid!2sid!4v1599321604439!5m2!1sid!2sid" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
                     </div>
-                    <div class="form-group">
-                        <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                        <div class="validate"></div>
-                    </div>
-                    <div class="mb-3">
-                        <div class="loading">Loading</div>
-                        <div class="error-message"></div>
-                        <div class="sent-message">Your message has been sent. Thank you!</div>
-                    </div>
-                    <div class="text-center"><button type="submit">Send Message</button></div>
-                </form>
+                </div>
             </div>
+
+            <div class="col-sm-6">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="info-box mb-4">
+                            <i class="bx bx-map" style="color: goldenrod;"></i>
+                            <h3>Site Office Address</h3>
+                            <p>JL. Bugis Raya No. 122 <br> Bawang - Tg. Priok <br> Jakarta Utara 14320 - Indonesia</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 ">
+                        <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.1131630457744!2d106.88512331432277!3d-6.115465195571883!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a1fc0357e5625%3A0x42f3659b3d8c036e!2sJl.%20Bugis%20No.122%2C%20RT.5%2FRW.1%2C%20Kb.%20Bawang%2C%20Tj.%20Priok%2C%20Kota%20Jkt%20Utara%2C%20Daerah%20Khusus%20Ibukota%20Jakarta%2014320!5e0!3m2!1sid!2sid!4v1601109502545!5m2!1sid!2sid" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
+                    </div>
+
+                </div>
+            </div>
+
+
+
+
 
         </div>
 
-        <div class="my-5">
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="info-box mb-4">
-                        <i class="bx bx-map" style="color: goldenrod;"></i>
-                        <h3>Site Office Address</h3>
-                        <p>JL. Bugis Raya No. 122 <br> Bawang - Tg. Priok <br> Jakarta Utara 14320 - Indonesia</p>
+        <div class="col-lg-6" style="display: none;">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                <div class="form-row">
+                    <div class="col form-group">
+                        <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <div class="validate"></div>
+                    </div>
+                    <div class="col form-group">
+                        <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                        <div class="validate"></div>
                     </div>
                 </div>
-            </div>
-
-            <div class="row">
-                <div class="col-lg-6 ">
-                    <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.1131630457744!2d106.88512331432277!3d-6.115465195571883!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a1fc0357e5625%3A0x42f3659b3d8c036e!2sJl.%20Bugis%20No.122%2C%20RT.5%2FRW.1%2C%20Kb.%20Bawang%2C%20Tj.%20Priok%2C%20Kota%20Jkt%20Utara%2C%20Daerah%20Khusus%20Ibukota%20Jakarta%2014320!5e0!3m2!1sid!2sid!4v1601109502545!5m2!1sid!2sid" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                    <div class="validate"></div>
                 </div>
-            </div>
-
+                <div class="form-group">
+                    <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                    <div class="validate"></div>
+                </div>
+                <div class="mb-3">
+                    <div class="loading">Loading</div>
+                    <div class="error-message"></div>
+                    <div class="sent-message">Your message has been sent. Thank you!</div>
+                </div>
+                <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
         </div>
 
     </div>
