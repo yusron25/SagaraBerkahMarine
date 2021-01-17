9529ec5dc8385ef85217ab24758cba80871c1054
yusron <yusron.m@dci-indonesia.com>
2021-01-17 13:42:06 +0700

    add footer navbar

--- application/views/_Layout.php
+++ application/views/_Layout.php
@@ -126,7 +126,7 @@
                 <nav class="mt-3 nav-menu d-none d-lg-block nav-white">
                     <ul>
                         <li><a href="<?= base_url() ?>/" data-id="hero">Home</a></li>
-                        <!-- <li><a href="<?= base_url() ?>/our-business" data-id="business">Our Business</a></li> -->
+                        <li><a href="<?= base_url() ?>/our-business" data-id="business">Our Business</a></li>
                         <li><a href="<?= base_url() ?>/our-services" data-id="our-services">Our Services</a></li>
                         <li><a href="<?= base_url() ?>/why-us" data-id="about">Why Us</a></li>
                         <li><a href="<?= base_url() ?>/contact" data-id="contact">Contact</a></li>
